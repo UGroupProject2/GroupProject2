a43a55269ecbc7759cb86e4dbb9619719345bd5d
snappyninja <snappyninja@gmail.com>
2018-08-04 11:18:12 -0600

    added models for user inventory and charecters

---
+++ models/charecter.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes) {
+    var Charecter = sequelize.define("Charecter", {
+      // Giving the Author model a name of type STRING
+      name: DataTypes.STRING,
+      race: DataTypes.STRING,
+      class: DataTypes.STRING,
+      gender: DataTypes.STRING,
+      attack: DataTypes.INT,
+      hitPoints: DataTypes.INT,
+      intellagence: DataTypes.INT,
+      dexterity: DataTypes.INT,  
+    });
+  
+    Charecter.associate = function(models) {
+      // Associating Author with Posts
+      // When an Author is deleted, also delete any associated Posts
+      Charecter.belongsTo(models.user, {
+        foreignKey: {
+            allowNull: false
+          }
+      });
+    };
+  
+    return Charecter;
+  };
--- models/example.js
+++ models/example.js
@@ -1,7 +1,25 @@
+// module.exports = function(sequelize, DataTypes) {
+//   var Example = sequelize.define("Example", {
+//     text: DataTypes.STRING,
+//     description: DataTypes.TEXT
+//   });
+//   return Example;
+// };
+
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var User = sequelize.define("user", {
+    // Giving the Author model a name of type STRING
+    name: DataTypes.STRING,
+    password: Datatypes.STRING
   });
-  return Example;
+
+  Author.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Author.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Author;
 };
