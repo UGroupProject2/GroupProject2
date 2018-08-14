6da2ea4d4fb1980cf7c6ae36b3e9a7106f98814d
Greg <tubatica@gmail.com>
2018-08-13 19:31:45 -0600

    final

--- models/index.js
+++ models/index.js
@@ -8,6 +8,19 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
+if (process.env.JAWS_URL){
+  connection = mysql.createConnection(process.env.JAWS_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: 'q57yawiwmnaw13d2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      user: 'awb8ssj1urdooo27',
+      password: 'j1b0ydpfcsosyy9b',
+      database: 'stc7rb4yzb769wwa'
+    });
+
+  };
+
+
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
